debe2fbbf43c9c1c3f883f57cafc564025152bd2
warminski <warminpiotr@gmail.com>
2019-10-23 15:30:32 +0200

    xd

---
+++ ConsoleApp1/ConsoleApp1/Car.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp1
+{
+    class Car
+    {
+        public string marka;
+        public int rok;
+
+        public Car(string marka, int rok)
+        {
+            this.marka = marka;
+            this.rok = rok;
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var car1 = new Car("Renault", 1999);
+            Console.WriteLine(car1.marka + ' ' + car1.rok);
+            Console.ReadKey();
+
+            /*var car2 = new Car("VW", 1997);
+            Console.WriteLine(car2.marka + ' ' + car2.rok);
+            Console.ReadKey();
+
+            car1 = car2;
+            Console.WriteLine(car2.marka + ' ' + car2.rok);
+            Console.ReadKey();*/
+
+        }
+    }
+}
