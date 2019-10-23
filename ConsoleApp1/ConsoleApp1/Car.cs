c21be1a22ce7ada9b7221e6e4ecb3a1b1bbfab24
warminski <warminpiotr@gmail.com>
2019-10-23 15:57:32 +0200

    xd

--- ConsoleApp1/ConsoleApp1/Car.cs
+++ ConsoleApp1/ConsoleApp1/Car.cs
@@ -4,11 +4,27 @@
     {
         public string marka;
         public int rok;
+        private string model;
+        private int iloscDrzwi;
+        private float pojemnoscSilnika;
+        public float srednieSpalanie;
 
         public Car(string marka, int rok)
         {
             this.marka = marka;
             this.rok = rok;
+        }
+
+        private double ObliczSpalanie(double dlugoscTrasy)
+        {
+            double spalanie = srednieSpalanie * dlugoscTrasy;
+            return spalanie;
+        }
+
+        public double ObliczKosztPrzejazdu(double dlugoscTrasy, double cenaPaliwa)
+        {
+            double kosztPrzejazdu = ObliczSpalanie(dlugoscTrasy) * cenaPaliwa;
+            return kosztPrzejazdu;
         }
     }
 }
